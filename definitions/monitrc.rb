e65393c80cfd6cfdcd41ae620bbaa7d11bc7a374
Greg Thornton <xdissent@me.com>
2012-04-19 01:38:04 -0500

    Remove action param from definition and enable by default

--- definitions/monitrc.rb
+++ definitions/monitrc.rb
@@ -1,18 +1,17 @@
 # reload      Reload monit so it notices the new service.  :delayed (default) or :immediately.
-define :monitrc, :action => :enable, :reload => :delayed do
+define :monitrc, :enable => true, :reload => :delayed do
   if params[:enable]
-    template "/etc/monit/conf.d/#{name}.conf" do
+    template "/etc/monit/conf.d/#{params[:name]}.conf" do
       owner "root"
       group "root"
       mode 0644
-      source "#{name}.conf.erb"
-      cookbook "monit"
+      source "#{params[:name]}.conf.erb"
       variables params
       notifies :restart, resources(:service => "monit"), params[:reload]
       action :create
     end
   else
-    template "/etc/monit/conf.d/#{name}.conf" do
+    template "/etc/monit/conf.d/#{params[:name]}.conf" do
       action :delete
       notifies :restart, resources(:service => "monit"), params[:reload]
     end
