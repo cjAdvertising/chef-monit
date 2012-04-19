7f95592c88cd53645abc194cafec594b0801575c
Greg Thornton <xdissent@me.com>
2012-04-19 00:42:18 -0500

    Adding more config attrs for the template

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,13 +1,25 @@
 default[:monit][:notify_email]          = "notify@example.com"
 
+default[:monit][:listen]                = "localhost"
+default[:monit][:allow]                 = ["localhost"]
+
 default[:monit][:poll_period]           = 60
 default[:monit][:poll_start_delay]      = 120
 
-default[:monit][:mail_format][:subject] = "$SERVICE $EVENT"
-default[:monit][:mail_format][:from]    = "monit@example.com"
-default[:monit][:mail_format][:message]    = <<-EOS
-Monit $ACTION $SERVICE at $DATE on $HOST: $DESCRIPTION.
-Yours sincerely,
-monit
+default[:monit][:basedir]               = "/var/lib/monit/events"
+default[:monit][:slots]                 = 100
+
+default[:monit][:mail_server]           = "localhost"
+default[:monit][:mail_format][:subject] = "monit alert --  $EVENT $SERVICE"
+default[:monit][:mail_format][:from]    = "monit@$HOST"
+default[:monit][:mail_format][:message] = <<-EOS
+$EVENT Service $SERVICE
+     Date:        $DATE
+     Action:      $ACTION
+     Host:        $HOST
+     Description: $DESCRIPTION
+
+Your faithful employee,
+Monit
 EOS
 
